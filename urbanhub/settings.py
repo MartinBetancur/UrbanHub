c243cf50a7b983968a21e100a4bc8acf0f0f6d0c
martinbetancur <martinjavieli@gmail.com>
2025-02-05 19:01:20 -0500

    Adding allowed host

--- urbanhub/settings.py
+++ urbanhub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l2emd%=iazo#k$2l6cl_vl^^$&e5966h6*!zm^$ww^h9&mhhx*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['urbanhub-riot.onrender.com']
 
 
 # Application definition
